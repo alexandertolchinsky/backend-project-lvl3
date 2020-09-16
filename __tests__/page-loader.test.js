2f1cbe2eae45018bbf5ecb8a07bdb2ba30e75325
Alexander Tolchinsky <bombur@kaap.io>
2020-09-16 17:25:21 +0300

    step1v3

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -2,7 +2,7 @@
  * @jest-environment node
  */
 import nock from 'nock';
-import { mkdtemp, readFile } from 'fs/promises';
+import { promises as fs } from 'fs';
 import { dirname, join } from 'path';
 import { tmpdir } from 'os';
 import { fileURLToPath } from 'url';
@@ -17,15 +17,15 @@ const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filen
 
 let tmpDir;
 beforeEach(async () => {
-  tmpDir = await mkdtemp(join(tmpdir(), 'page-loader-'));
+  tmpDir = await fs.mkdtemp(join(tmpdir(), 'page-loader-'));
 });
 
 test('pageLoader', async () => {
-  const correctAnswer = await readFile(getFixturePath('ru-hexlet-io-courses.html'), 'utf-8');
+  const correctAnswer = await fs.readFile(getFixturePath('ru-hexlet-io-courses.html'), 'utf-8');
   nock('https://ru.hexlet.io')
     .get('/courses')
     .reply(200, correctAnswer);
   await pageLoader(tmpDir, 'https://ru.hexlet.io/courses');
-  const answer = await readFile(`${tmpDir}/ru-hexlet-io-courses.html`, 'utf-8');
+  const answer = await fs.readFile(`${tmpDir}/ru-hexlet-io-courses.html`, 'utf-8');
   expect(answer).toBe(correctAnswer);
 });
